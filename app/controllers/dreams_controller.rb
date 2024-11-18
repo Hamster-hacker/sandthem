d9363404ad6955f894460855a344c85f9e35f35d
Anastasia Polovinkina <apolovika@gmail.com>
2024-11-18 17:25:33 +0100

    categories new view and form

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -3,4 +3,28 @@ class DreamsController < ApplicationController
   def index
     @dreams = Dream.all
   end
+
+
+  def new
+    @dream = Dream.new
+
+  end
+
+  def create
+    @dream = Dream.new(dream_params)
+    @dream.user = current_user
+
+    if @dream.save
+      redirect_to @dream, notice: "dream was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+
+  private
+  def dream_params
+    params.require(:dream).permit(:title, :description, :category, :price, :image_url, :duration, :rating)
+  end
 end
--- app/models/dream.rb
+++ app/models/dream.rb
@@ -10,4 +10,10 @@ class Dream < ApplicationRecord
   validates :price, numericality: { greater_than_or_equal_to: 0 }
   validates :duration, numericality: { only_integer: true, greater_than: 0 }
   validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+
+
+  CATEGORIES = ["Adventure", "Fantasy", "Mystery", "Romance", "Horror", "Sci-Fi", "Comedy"]
+
+  # Validate that the category is included in the list
+  validates :category, inclusion: { in: CATEGORIES}
 end
