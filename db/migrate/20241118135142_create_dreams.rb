8f1c98ca6bcc1aa447fedafc53fa2d6d2fee47cd
Anastasia Polovinkina <apolovika@gmail.com>
2024-11-18 14:56:13 +0100

    first part of the setup

---
+++ db/migrate/20241118135142_create_dreams.rb
@@ -0,0 +1,16 @@
+class CreateDreams < ActiveRecord::Migration[7.1]
+  def change
+    create_table :dreams do |t|
+      t.string :title
+      t.text :description
+      t.string :category
+      t.string :image_url
+      t.decimal :price
+      t.integer :duration
+      t.decimal :rating
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_18_135235) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "dream_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["dream_id"], name: "index_bookings_on_dream_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "dreams", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "category"
+    t.string "image_url"
+    t.decimal "price"
+    t.integer "duration"
+    t.decimal "rating"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_dreams_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "bookings", "dreams"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "dreams", "users"
 end
