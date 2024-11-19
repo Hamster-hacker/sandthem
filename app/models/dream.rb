7076a37781747c55f1de8727542e4ffb8fdf253d
Anurag Phalke <anurag.phalke@gmail.com>
2024-11-19 16:20:35 +0100

    form validation error messages corrected (create dream)

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -6,18 +6,17 @@ class Dream < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :reviewers, through: :reviews, source: :user
 
-  validates :title, presence: true, length: { maximum: 120 }
-  validates :description, presence: true
-  validates :category, presence: true
-  validates :price, presence: true
-  validates :image_url, presence: true
-  validates :price, numericality: { greater_than_or_equal_to: 0 }
-  validates :duration, numericality: { only_integer: true, greater_than: 0 }
-  validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
-
   CATEGORIES = ["Adventure", "Fantasy", "Mystery", "Romance", "Horror", "Sci-Fi", "Comedy"]
 
-  validates :category, inclusion: { in: CATEGORIES }
+  validates :title, presence: true, length: { maximum: 120 }
+  validates :description, presence: true
+  validates :category, presence: true, allow_blank: true
+  validates :category, inclusion: { in: CATEGORIES, message: " invalid. Please select a valid category" }
+  validates :price, presence: { message: " can't be blank" }
+  validates :image_url, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }, allow_blank: true
+  validates :duration, numericality: { only_integer: true, greater_than: 0, message: " must be a valid number" }
+  validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
 
   def is_owner?(user)
     self.user == user
