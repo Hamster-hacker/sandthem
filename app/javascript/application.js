aaa0940e2d55022d2c167bbda690153b381f479b
tuana erol <tuanaerol59@gmail.com>
2024-11-21 10:52:16 +0100

    Revert "Create dream form is been styled."
    This reverts commit 3bc703887da971b68c1aacc2ca988979777470f8.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,16 +3,3 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
-
-document.addEventListener("turbo:load", () => {
-  flatpickr(".datepicker", {
-    mode: "range",
-    dateFormat: "d-m-Y",
-    altInput: true,
-    altFormat: "F j, Y",
-    minDate: "today",
-    maxDate: new Date().fp_incr(365)
-  });
-});
